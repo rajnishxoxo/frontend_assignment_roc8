a9c6668ed19c686469cc0819790ad4df7c9f2c0f
Rajnish. R. Singh Rajput <64491136+rajnishxoxo@users.noreply.github.com>
2024-01-18 22:10:36 +0530

    Added Redux store Provided Store to App

--- src/Component/SearchBar.jsx
+++ src/Component/SearchBar.jsx
@@ -23,9 +23,10 @@ const SearchBar = () => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        
+
         const data = await response.json();
         console.log(data);
+        
       } catch (error) {
         console.error("Error fetching data:", error.message);
       }

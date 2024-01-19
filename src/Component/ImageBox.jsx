ba7b6d848e2b154fe4e476de8ffabecd924409ca
Rajnish. R. Singh Rajput <64491136+rajnishxoxo@users.noreply.github.com>
2024-01-19 21:14:08 +0530

    Fixed Css issue

--- src/Component/ImageBox.jsx
+++ src/Component/ImageBox.jsx
@@ -7,24 +7,51 @@ const ImageBox = () => {
   const flatArray = imageData.flat(Infinity);
   const shuffledArray = flatArray.sort(() => Math.random() - 0.5);
   const selectedData = shuffledArray.slice(0, 6);
+  //HardCoded Data
+  const shortCutArray = [
+    "Digital",
+    "Computer",
+    "Codierung",
+    "Tech",
+    "Netz",
+    "Code",
+    "Finanzieren",
+    "Marketing",
+  ];
 
-  // Check if imageData is an array and if it has elements
   if (!Array.isArray(imageData) || imageData.length === 0) {
     return;
   }
 
   return (
-    <div className=" w-full flex flex-col  lg:w-[1287px] lg:h-[950px] lg:grid grid-cols-3 gap-4 lg:m-auto rounded-[8.878px] bg-white">
-      {selectedData &&
-        selectedData.flat().map((data) => {
-          const { id, webformatURL, tags } = data;
-
+    <div className="m-auto border bg-gray-200 items-center">
+      <div
+        className=" w-full py-3 m-auto flex flex-row justify-between  lg:w-[1287px]"
+        style={{
+          backgroundColor: "#F5F5F5",
+        }}
+      >
+        {shortCutArray.map((data) => {
           return (
-            <div key={id}>
-              <ImageCard data={{ id, webformatURL, tags }} />
+            <div>
+              <button className=" border border-gray-400  px-6 rounded-md py-2 m-2">
+                {data}
+              </button>
             </div>
           );
         })}
+      </div>
+      <div className=" w-full  flex flex-col  lg:w-[1287px]  lg:grid lg:grid-cols-3 gap-2   lg:m-auto rounded-[8.878px] bg-white">
+        {selectedData &&
+          selectedData.flat().map((data) => {
+            const { id, webformatURL, tags } = data;
+            return (
+              <div key={id}>
+                <ImageCard data={{ id, webformatURL, tags }} />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
--- src/Component/ImageCard.jsx
+++ src/Component/ImageCard.jsx
@@ -1,22 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ImageCard = (props) => {
   const { id, webformatURL, tags } = props.data;
   // tags into array.
-
   const arrayOfTags = tags.split(",");
-  
+
   return (
-    <div className=" w-[300px] h-[150px] m-auto mt-2  lg:w-[350px] lg:h-[240px] lg:m-auto lg:mt-5  ">
-      <img className="rounded-md w-full h-3/4 object-cover" src={webformatURL} alt="Image" />
-      <div className="flex flex-row  justify-start  mt-2  items-center">
-        {arrayOfTags.map((data) => {
-          return (
-            <div className="text-white  mr-4  px-2 py-1 font-helvetica-neue text-xs font-normal uppercase leading-5 bg-slate-400">
-              <p>{data}</p>
-            </div>
-          );
-        })}
+    <div className="w-[300px] h-[150px] m-auto mt-2 lg:w-[350px] lg:h-[240px] lg:m-auto lg:mt-5">
+      <img
+        className="rounded-md w-full h-3/4 object-cover"
+        src={webformatURL}
+        alt="Image"
+      />
+      <div className="flex flex-row  justify-between mt-2 items-center">
+        {arrayOfTags.map((data) => (
+          <div
+            key={data}
+            className="text-white w-20  mr-2 px-2 py-1 font-helvetica-neue text-xs font-normal uppercase leading-5 bg-slate-400"
+          >
+            <p>{data}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
